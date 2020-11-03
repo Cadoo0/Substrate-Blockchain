41a96e45360a133431af28e731e2b2da1c46f052
Ricardo Kamerman <ricardokamerman@gmail.com>
2020-11-03 17:07:21 +0100

    Port aangepast

--- substrate-node-template/helpers/node-3.sh
+++ substrate-node-template/helpers/node-3.sh
@@ -1,1 +1,1 @@
-../target/release/node-template --base-path $(pwd)/../../node-03 --chain ../customSpecRaw.json --port 30334 --ws-port 9945 --rpc-port 9934 --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' --rpc-methods=Unsafe --name MyNode03 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/$1
+../target/release/node-template --base-path $(pwd)/../../node-03 --chain ../customSpecRaw.json --port 30335 --ws-port 9946 --rpc-port 9935 --telemetry-url 'wss://telemetry.polkadot.io/submit/ 0' --rpc-methods=Unsafe --name MyNode03 --bootnodes /ip4/127.0.0.1/tcp/30333/p2p/$1
